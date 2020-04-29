6716b2678e1dee4606c7242550ecbaa08e4d8b60
Colin Marsch <colinmarsch@gmail.com>
2020-04-28 21:03:29 -0500

    Fixed 30 second notif not being cancelled and streak breaking on lock

--- app/src/main/java/me/colinmarsch/dawn/InAppActivity.kt
+++ app/src/main/java/me/colinmarsch/dawn/InAppActivity.kt
@@ -1,17 +1,20 @@
 package me.colinmarsch.dawn
 
 import android.app.AlarmManager
+import android.app.AlarmManager.RTC_WAKEUP
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.PowerManager
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import me.colinmarsch.dawn.NotificationHelper.Companion.BROKE_STREAK_NOTIF_ID
+import me.colinmarsch.dawn.NotificationHelper.Companion.STAY_ALARM_ID
 import me.colinmarsch.dawn.NotificationHelper.Companion.STAY_IN_APP_ID
 import me.colinmarsch.dawn.NotificationHelper.Companion.STAY_NOTIF_ID
 import me.colinmarsch.dawn.NotificationHelper.Companion.SUCCESS_STREAK_ALARM_ID
@@ -39,28 +42,33 @@ class InAppActivity : AppCompatActivity() {
     }
 
     override fun onPause() {
+        // TODO(colinmarsch) this still runs after the time completes, it shouldn't
+        // Don't want to break the streak if the activity is paused due to locking the phone
+        val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+        if (powerManager.isInteractive) {
+            NotificationHelper.createNotificationChannel(applicationContext)
+            val builder = NotificationCompat.Builder(this, NotificationHelper.CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground) // TODO(colinmarsch) update the icon
+                .setContentTitle("Dawn")
+                .setContentText("You left Dawn and broke your streak!")
+
+            with(NotificationManagerCompat.from(applicationContext)) {
+                notify(BROKE_STREAK_NOTIF_ID, builder.build())
+            }
+
+            with(sharedPref.edit()) {
+                putInt(getString(R.string.saved_streak_key), 0)
+                apply()
+            }
+
+            // Cancel the alarm from the countdown
+            val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            val successStreakIntent = Intent(this, AlarmReceiver::class.java)
+            successStreakIntent.putExtra("CASE", "STREAK")
+            val pendingIntent = PendingIntent.getBroadcast(this, SUCCESS_STREAK_ALARM_ID, successStreakIntent, 0)
+            alarmManager.cancel(pendingIntent)
+        }
         super.onPause()
-        NotificationHelper.createNotificationChannel(applicationContext)
-        val builder = NotificationCompat.Builder(this, NotificationHelper.CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_foreground) // TODO(colinmarsch) update the icon
-            .setContentTitle("Dawn")
-            .setContentText("You left Dawn and broke your streak!")
-
-        with(NotificationManagerCompat.from(applicationContext)) {
-            notify(BROKE_STREAK_NOTIF_ID, builder.build())
-        }
-
-        with(sharedPref.edit()) {
-            putInt(getString(R.string.saved_streak_key), 0)
-            apply()
-        }
-
-        // Cancel the alarm from the countdown
-        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val successStreakIntent = Intent(this, AlarmReceiver::class.java)
-        successStreakIntent.putExtra("CASE", "STREAK")
-        val pendingIntent = PendingIntent.getBroadcast(this, SUCCESS_STREAK_ALARM_ID, successStreakIntent, 0)
-        alarmManager.cancel(pendingIntent)
     }
 
     private fun startCountdown() {
@@ -72,12 +80,13 @@ class InAppActivity : AppCompatActivity() {
         val successStreakIntent = Intent(this, AlarmReceiver::class.java)
         successStreakIntent.putExtra("CASE", "STREAK")
         val pendingIntent = PendingIntent.getBroadcast(this, SUCCESS_STREAK_ALARM_ID, successStreakIntent, 0)
-        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, whenTime, pendingIntent)
+        alarmManager.setExactAndAllowWhileIdle(RTC_WAKEUP, whenTime, pendingIntent)
 
         object : CountDownTimer(totalTime, interval) {
             override fun onTick(millisUntilFinished: Long) {
                 val minutes = millisUntilFinished / 60000L
                 val seconds = (millisUntilFinished % 60000L) / 1000L
+                // TODO(colinmarsch) need the same single digit fix here that was in the MainActivity notification
                 countDownTimerText.text = "$minutes:$seconds" // TODO(colinmarsch) fix this string behaviour
             }
 
@@ -88,11 +97,12 @@ class InAppActivity : AppCompatActivity() {
     }
 
     private fun cancelAlarmAndNotif() {
-        val inAppIntent = Intent(this, InAppActivity::class.java)
-        val dupIntent = PendingIntent.getBroadcast(this, STAY_IN_APP_ID, inAppIntent, 0)
+        val inAppIntent = Intent(this, AlarmReceiver::class.java)
+        val dupIntent = PendingIntent.getBroadcast(this, STAY_ALARM_ID, inAppIntent, 0)
         (getSystemService(Context.ALARM_SERVICE) as AlarmManager).also {
-            it.setAlarmClock(
-                AlarmManager.AlarmClockInfo(System.currentTimeMillis() + 1000L, dupIntent),
+            it.setExactAndAllowWhileIdle(
+                RTC_WAKEUP,
+                System.currentTimeMillis() + 1000L,
                 dupIntent
             )
             it.cancel(dupIntent)
