e03c8a03e96c8a0f18e6d7a01e713d9d2140be4d
Colin Marsch <colinmarsch@gmail.com>
2020-09-02 21:16:26 -0500

    Switch toggle with a button for setting the alarm

--- app/src/main/java/me/colinmarsch/dawn/StayOffActivity.kt
+++ app/src/main/java/me/colinmarsch/dawn/StayOffActivity.kt
@@ -7,9 +7,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.NumberPicker
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SwitchCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import java.util.*
@@ -17,7 +17,7 @@ import java.util.*
 class StayOffActivity : AppCompatActivity() {
     private lateinit var alarmManager: AlarmManager
     private lateinit var stayOffTimePicker: NumberPicker
-    private lateinit var toggleButton: SwitchCompat
+    private lateinit var toggleButton: Button
     private var pendingIntent: PendingIntent? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,94 +37,73 @@ class StayOffActivity : AppCompatActivity() {
         stayOffTimePicker.value = stayOffMinutes
     }
 
-    override fun onResume() {
-        super.onResume()
-        toggleButton.isChecked = alarmManager.nextAlarmClock != null
-    }
-
-    fun onToggleClicked(view: View) {
-        val toggle = view as SwitchCompat
+    fun onSetAlarmClicked(view: View) {
         val alarmIntent = Intent(this, AlarmReceiver::class.java)
         alarmIntent.putExtra("CASE", "ALARM")
         val mainIntent = Intent(this, MainActivity::class.java)
-        if (toggle.isChecked) {
-            val sharedPrefs =
-                getSharedPreferences(getString(R.string.shared_prefs_name), Context.MODE_PRIVATE)
-            val calendar = Calendar.getInstance()
-            calendar.set(
-                Calendar.HOUR_OF_DAY,
-                sharedPrefs.getInt(getString(R.string.saved_hour_key), 0)
-            )
-            calendar.set(
-                Calendar.MINUTE,
-                sharedPrefs.getInt(getString(R.string.saved_minute_key), 0)
-            )
-            calendar.set(Calendar.SECOND, 0)
-            if (calendar.timeInMillis < System.currentTimeMillis()) {
-                calendar.add(Calendar.DATE, 1)
-            }
-
-            val hour = calendar.hourText()
-            val minute = calendar.minuteText()
-            val amPM = calendar.get(Calendar.AM_PM) == Calendar.AM
-            val contentText = if (amPM) {
-                "Alarm set for $hour:$minute AM"
-            } else {
-                "Alarm set for $hour:$minute PM"
-            }
-
-            val alarmDismissIntent = Intent(this, AlarmReceiver::class.java).also {
-                it.putExtra("CASE", "DISMISS")
-            }
-            val pendingDismissIntent = PendingIntent.getBroadcast(
-                this,
-                NotificationHelper.DISMISS_ALARM_ID,
-                alarmDismissIntent,
-                0
-            )
-
-            val pendingMainIntent =
-                PendingIntent.getActivity(this, NotificationHelper.TIME_NOTIF_ID, mainIntent, 0)
-            NotificationHelper.createNotificationChannel(applicationContext)
-            val builder = NotificationCompat.Builder(view.context, NotificationHelper.CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground) // TODO(colinmarsch) update the icon
-                .setContentTitle("Dawn")
-                .setContentText(contentText)
-                .setContentIntent(pendingMainIntent)
-                .setCategory(NotificationCompat.CATEGORY_REMINDER)
-                .setOngoing(true)
-                // TODO(colinmarsch) need a real dismiss icon here
-                .addAction(R.drawable.ic_launcher_foreground, "Dismiss", pendingDismissIntent)
-
-            with(NotificationManagerCompat.from(applicationContext)) {
-                notify(NotificationHelper.TIME_NOTIF_ID, builder.build())
-            }
-
-            pendingIntent =
-                PendingIntent.getBroadcast(this, NotificationHelper.ALARM_ID, alarmIntent, 0)
-            alarmManager.setAlarmClock(
-                AlarmManager.AlarmClockInfo(
-                    calendar.timeInMillis,
-                    pendingIntent
-                ), pendingIntent
-            )
-            Log.d("DAWN", "Started the alarm for $calendar")
-
-            setStayOffTime()
-        } else {
-            // This intent was made to be able to cancel the alarm after the app has been closed
-            // it matches the original intent that was used to create the alarm
-            val dupIntent =
-                PendingIntent.getBroadcast(this, NotificationHelper.ALARM_ID, alarmIntent, 0)
-            alarmManager.setAlarmClock(
-                AlarmManager.AlarmClockInfo(System.currentTimeMillis() + 1000L, dupIntent),
-                dupIntent
-            )
-            with(NotificationManagerCompat.from(applicationContext)) {
-                cancel(NotificationHelper.TIME_NOTIF_ID)
-            }
-            alarmManager.cancel(dupIntent)
+        val sharedPrefs =
+            getSharedPreferences(getString(R.string.shared_prefs_name), Context.MODE_PRIVATE)
+        val calendar = Calendar.getInstance()
+        calendar.set(
+            Calendar.HOUR_OF_DAY,
+            sharedPrefs.getInt(getString(R.string.saved_hour_key), 0)
+        )
+        calendar.set(
+            Calendar.MINUTE,
+            sharedPrefs.getInt(getString(R.string.saved_minute_key), 0)
+        )
+        calendar.set(Calendar.SECOND, 0)
+        if (calendar.timeInMillis < System.currentTimeMillis()) {
+            calendar.add(Calendar.DATE, 1)
         }
+
+        val hour = calendar.hourText()
+        val minute = calendar.minuteText()
+        val amPM = calendar.get(Calendar.AM_PM) == Calendar.AM
+        val contentText = if (amPM) {
+            "Alarm set for $hour:$minute AM"
+        } else {
+            "Alarm set for $hour:$minute PM"
+        }
+
+        val alarmDismissIntent = Intent(this, AlarmReceiver::class.java).also {
+            it.putExtra("CASE", "DISMISS")
+        }
+        val pendingDismissIntent = PendingIntent.getBroadcast(
+            this,
+            NotificationHelper.DISMISS_ALARM_ID,
+            alarmDismissIntent,
+            0
+        )
+
+        val pendingMainIntent =
+            PendingIntent.getActivity(this, NotificationHelper.TIME_NOTIF_ID, mainIntent, 0)
+        NotificationHelper.createNotificationChannel(applicationContext)
+        val builder = NotificationCompat.Builder(view.context, NotificationHelper.CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground) // TODO(colinmarsch) update the icon
+            .setContentTitle("Dawn")
+            .setContentText(contentText)
+            .setContentIntent(pendingMainIntent)
+            .setCategory(NotificationCompat.CATEGORY_REMINDER)
+            .setOngoing(true)
+            // TODO(colinmarsch) need a real dismiss icon here
+            .addAction(R.drawable.ic_launcher_foreground, "Dismiss", pendingDismissIntent)
+
+        with(NotificationManagerCompat.from(applicationContext)) {
+            notify(NotificationHelper.TIME_NOTIF_ID, builder.build())
+        }
+
+        pendingIntent =
+            PendingIntent.getBroadcast(this, NotificationHelper.ALARM_ID, alarmIntent, 0)
+        alarmManager.setAlarmClock(
+            AlarmManager.AlarmClockInfo(
+                calendar.timeInMillis,
+                pendingIntent
+            ), pendingIntent
+        )
+        Log.d("DAWN", "Started the alarm for $calendar")
+
+        setStayOffTime()
     }
 
     private fun Calendar.hourText() =
