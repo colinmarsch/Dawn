99a007c2fc99a6315c729dacc6dedf01f6644450
Colin Marsch <colinmarsch@gmail.com>
2020-11-06 23:33:10 -0500

    Update calendar UI with colors for the days that are success and failues

--- app/src/main/java/me/colinmarsch/dawn/DayViewContainer.kt
+++ app/src/main/java/me/colinmarsch/dawn/DayViewContainer.kt
@@ -1,12 +1,17 @@
 package me.colinmarsch.dawn
 
+import android.content.Context
+import android.graphics.Color
 import android.view.View
 import android.widget.TextView
 import com.kizitonwose.calendarview.model.CalendarDay
 import com.kizitonwose.calendarview.model.CalendarMonth
+import com.kizitonwose.calendarview.model.DayOwner
 import com.kizitonwose.calendarview.ui.DayBinder
 import com.kizitonwose.calendarview.ui.MonthHeaderFooterBinder
 import com.kizitonwose.calendarview.ui.ViewContainer
+import java.text.SimpleDateFormat
+import java.time.ZoneId
 import java.time.format.TextStyle
 import java.util.*
 
@@ -24,7 +29,47 @@ object DayBinder : DayBinder<DayViewContainer> {
 
     // Called every time we need to reuse a container.
     override fun bind(container: DayViewContainer, day: CalendarDay) {
+        val context = container.view.context
         container.textView.text = day.date.dayOfMonth.toString()
+        if (day.owner == DayOwner.THIS_MONTH) {
+            container.textView.setTextColor(Color.BLACK)
+        } else {
+            container.textView.setTextColor(Color.GRAY)
+        }
+
+        val sharedPrefs = context.getSharedPreferences(
+            context.getString(R.string.shared_prefs_name),
+            Context.MODE_PRIVATE
+        )
+        val df = SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault())
+
+        val newSuccessfulDaysSet: HashSet<String> = HashSet(
+            sharedPrefs.getStringSet(
+                context.getString(R.string.successful_days_key),
+                HashSet<String>()
+            )
+        )
+        val successToday = newSuccessfulDaysSet.find { success ->
+            val date = df.parse(success).toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
+            return@find date == day.date
+        }
+
+        if (successToday != null) {
+            container.view.setBackgroundColor(Color.GREEN)
+        } else {
+            val newFailedDaysSet: HashSet<String> = HashSet(
+                sharedPrefs.getStringSet(
+                    context.getString(R.string.failed_days_key),
+                    HashSet<String>()
+                )
+            )
+            newFailedDaysSet.find { failed ->
+                val date = df.parse(failed).toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
+                return@find date == day.date
+            }?.let {
+                container.view.setBackgroundColor(Color.RED)
+            }
+        }
     }
 }
 
