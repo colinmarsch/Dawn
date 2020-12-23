d713c0542066c2268344085e1439837748e42d91
Colin Marsch <colinmarsch@gmail.com>
2020-12-23 17:54:45 -0600

    Move UI logic from MainActivity to MainFragment
    First step to migrating app to use only one activity.

--- app/src/main/java/me/colinmarsch/dawn/MainActivity.kt
+++ app/src/main/java/me/colinmarsch/dawn/MainActivity.kt
@@ -1,139 +1,21 @@
 package me.colinmarsch.dawn
 
-import android.app.AlarmManager
-import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
-import android.media.RingtoneManager.*
-import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var alarmManager: AlarmManager
-    private lateinit var timePicker: TimePicker
-    private lateinit var nextButton: Button
-    private lateinit var ringtoneButton: Button
-    private lateinit var ringtoneLabel: TextView
-    private lateinit var ringtoneVolume: SeekBar
-
-    private lateinit var sharedPrefs: SharedPreferences
-
-    private var previewPlaying = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_content_host)
 
-        sharedPrefs = getSharedPreferences(
-            getString(R.string.shared_prefs_name),
-            Context.MODE_PRIVATE
-        )
-
-        timePicker = findViewById(R.id.alarm_time_picker)
-        timePicker.setOnTimeChangedListener { _, hour, minute ->
-            with(sharedPrefs.edit()) {
-                putInt(getString(R.string.saved_hour_key), hour)
-                putInt(getString(R.string.saved_minute_key), minute)
-                apply()
-            }
-        }
-        nextButton = findViewById(R.id.choose_alarm_time_button)
-        nextButton.setOnClickListener {
-            if (!MediaHandler.validRingtoneSet(this)) {
-                Toast.makeText(this, getString(R.string.switch_ringtone), Toast.LENGTH_LONG).show()
-            } else {
-                val intent = Intent(this, GetUpDelayActivity::class.java)
-                startActivity(intent)
-            }
-        }
-        ringtoneButton = findViewById(R.id.choose_ringtone_button)
-        ringtoneButton.setOnClickListener {
-            val ringtoneIntent = Intent(ACTION_RINGTONE_PICKER)
-            ringtoneIntent.putExtra(EXTRA_RINGTONE_TITLE, "Select ringtone for alarm:")
-            ringtoneIntent.putExtra(EXTRA_RINGTONE_SHOW_SILENT, false)
-            ringtoneIntent.putExtra(EXTRA_RINGTONE_SHOW_DEFAULT, true)
-            ringtoneIntent.putExtra(EXTRA_RINGTONE_TYPE, TYPE_ALARM)
-            startActivityForResult(ringtoneIntent, 1)
-        }
-        ringtoneLabel = findViewById(R.id.ringtone_label)
-        val currentRingtoneTitle =
-            sharedPrefs.getString(getString(R.string.saved_ringtone_title_key), "Default")
-        ringtoneLabel.text = getString(R.string.current_ringtone, currentRingtoneTitle)
-
-        ringtoneVolume = findViewById(R.id.ringtone_volume_slider)
-        ringtoneVolume.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {}
-
-            override fun onStartTrackingTouch(seekBar: SeekBar) {}
-
-            override fun onStopTrackingTouch(seekBar: SeekBar) {
-                with(sharedPrefs.edit()) {
-                    putInt(getString(R.string.saved_volume_key), ringtoneVolume.progress)
-                    apply()
-                }
-                if (!previewPlaying && seekBar.progress != 0) {
-                    MediaHandler.startAlarm(this@MainActivity)
-                    previewPlaying = true
-                    seekBar.postDelayed({
-                        MediaHandler.stopAlarm()
-                        previewPlaying = false
-                    }, 2000)
-                }
-            }
-        })
-
-        alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == RESULT_OK) {
-            val uri: Uri? = data?.getParcelableExtra(EXTRA_RINGTONE_PICKED_URI)
-            uri?.let {
-                val ringtonePath = uri.toString()
-                with(sharedPrefs.edit()) {
-                    putString(getString(R.string.saved_ringtone_key), ringtonePath)
-                    apply()
-                }
-            }
-            val ringtone = getRingtone(this, uri)
-            val title = ringtone.getTitle(this)
-            ringtoneLabel.text = getString(R.string.current_ringtone, title)
-
-            with(sharedPrefs.edit()) {
-                putString(getString(R.string.saved_ringtone_title_key), title)
-                apply()
-            }
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        ringtoneVolume.progress = sharedPrefs.getInt(getString(R.string.saved_volume_key), 0)
-
-        if (alarmManager.nextAlarmClock != null) {
-            val nextAlarm = alarmManager.nextAlarmClock
-            val time = Calendar.getInstance()
-            time.timeInMillis = nextAlarm.triggerTime
-            timePicker.hour = time.get(Calendar.HOUR_OF_DAY)
-            timePicker.minute = time.get(Calendar.MINUTE)
-        } else {
-            val currentTime = Calendar.getInstance()
-            val hour = sharedPrefs.getInt(
-                getString(R.string.saved_hour_key),
-                currentTime.get(Calendar.HOUR_OF_DAY)
-            )
-            val minute = sharedPrefs.getInt(
-                getString(R.string.saved_minute_key),
-                currentTime.get(Calendar.MINUTE)
-            )
-            timePicker.hour = hour
-            timePicker.minute = minute
+        if (savedInstanceState == null) {
+            val fragment = MainFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.main_content, fragment)
+                .commit()
         }
     }
 
---
+++ app/src/main/java/me/colinmarsch/dawn/MainFragment.kt
@@ -0,0 +1,150 @@
+package me.colinmarsch.dawn
+
+import android.app.AlarmManager
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import java.util.*
+
+class MainFragment : Fragment() {
+    private lateinit var alarmManager: AlarmManager
+    private lateinit var timePicker: TimePicker
+    private lateinit var nextButton: Button
+    private lateinit var ringtoneButton: Button
+    private lateinit var ringtoneLabel: TextView
+    private lateinit var ringtoneVolume: SeekBar
+
+    private lateinit var sharedPrefs: SharedPreferences
+
+    private var previewPlaying = false
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.activity_main, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sharedPrefs = view.context.getSharedPreferences(
+            getString(R.string.shared_prefs_name),
+            Context.MODE_PRIVATE
+        )
+
+        timePicker = view.findViewById(R.id.alarm_time_picker)
+        timePicker.setOnTimeChangedListener { _, hour, minute ->
+            with(sharedPrefs.edit()) {
+                putInt(getString(R.string.saved_hour_key), hour)
+                putInt(getString(R.string.saved_minute_key), minute)
+                apply()
+            }
+        }
+        nextButton = view.findViewById(R.id.choose_alarm_time_button)
+        nextButton.setOnClickListener {
+            if (!MediaHandler.validRingtoneSet(view.context)) {
+                Toast.makeText(view.context, getString(R.string.switch_ringtone), Toast.LENGTH_LONG)
+                    .show()
+            } else {
+                // TODO(colinmarsch): move this navigation logic to the activity
+                val intent = Intent(view.context, GetUpDelayActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        ringtoneButton = view.findViewById(R.id.choose_ringtone_button)
+        ringtoneButton.setOnClickListener {
+            val ringtoneIntent = Intent(RingtoneManager.ACTION_RINGTONE_PICKER)
+            ringtoneIntent.putExtra(
+                RingtoneManager.EXTRA_RINGTONE_TITLE,
+                "Select ringtone for alarm:"
+            )
+            ringtoneIntent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_SILENT, false)
+            ringtoneIntent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_DEFAULT, true)
+            ringtoneIntent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_ALARM)
+            startActivityForResult(ringtoneIntent, 1)
+        }
+        ringtoneLabel = view.findViewById(R.id.ringtone_label)
+        val currentRingtoneTitle =
+            sharedPrefs.getString(getString(R.string.saved_ringtone_title_key), "Default")
+        ringtoneLabel.text = getString(R.string.current_ringtone, currentRingtoneTitle)
+
+        ringtoneVolume = view.findViewById(R.id.ringtone_volume_slider)
+        ringtoneVolume.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {}
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                with(sharedPrefs.edit()) {
+                    putInt(getString(R.string.saved_volume_key), ringtoneVolume.progress)
+                    apply()
+                }
+                if (!previewPlaying && seekBar.progress != 0) {
+                    MediaHandler.startAlarm(view.context)
+                    previewPlaying = true
+                    seekBar.postDelayed({
+                        MediaHandler.stopAlarm()
+                        previewPlaying = false
+                    }, 2000)
+                }
+            }
+        })
+
+        alarmManager = view.context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+    }
+
+    override fun onResume() {
+        super.onResume()
+        ringtoneVolume.progress = sharedPrefs.getInt(getString(R.string.saved_volume_key), 0)
+
+        if (alarmManager.nextAlarmClock != null) {
+            val nextAlarm = alarmManager.nextAlarmClock
+            val time = Calendar.getInstance()
+            time.timeInMillis = nextAlarm.triggerTime
+            timePicker.hour = time.get(Calendar.HOUR_OF_DAY)
+            timePicker.minute = time.get(Calendar.MINUTE)
+        } else {
+            val currentTime = Calendar.getInstance()
+            val hour = sharedPrefs.getInt(
+                getString(R.string.saved_hour_key),
+                currentTime.get(Calendar.HOUR_OF_DAY)
+            )
+            val minute = sharedPrefs.getInt(
+                getString(R.string.saved_minute_key),
+                currentTime.get(Calendar.MINUTE)
+            )
+            timePicker.hour = hour
+            timePicker.minute = minute
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == AppCompatActivity.RESULT_OK) {
+            val uri: Uri? = data?.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI)
+            uri?.let {
+                val ringtonePath = uri.toString()
+                with(sharedPrefs.edit()) {
+                    putString(getString(R.string.saved_ringtone_key), ringtonePath)
+                    apply()
+                }
+            }
+            val ringtone = RingtoneManager.getRingtone(context, uri)
+            val title = ringtone.getTitle(context)
+            ringtoneLabel.text = getString(R.string.current_ringtone, title)
+
+            with(sharedPrefs.edit()) {
+                putString(getString(R.string.saved_ringtone_title_key), title)
+                apply()
+            }
+        }
+    }
+}
