982474b3bc6ecf70b3beb0902c8bcc8dc82af994
Colin Marsch <colinmarsch@gmail.com>
2020-12-23 18:41:04 -0600

    Move UI logic from GetUpDelayActivity to GetUpDelayFragment

--- app/src/main/java/me/colinmarsch/dawn/GetUpDelayActivity.kt
+++ app/src/main/java/me/colinmarsch/dawn/GetUpDelayActivity.kt
@@ -1,43 +1,23 @@
 package me.colinmarsch.dawn
 
-import android.app.AlarmManager
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.NumberPicker
 import androidx.appcompat.app.AppCompatActivity
 
 class GetUpDelayActivity : AppCompatActivity() {
-    private lateinit var alarmManager: AlarmManager
-    private lateinit var getUpDelayPicker: NumberPicker
-    private lateinit var nextButton: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.getup_delay_layout)
-
-        alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val sharedPrefs =
-            getSharedPreferences(getString(R.string.shared_prefs_name), Context.MODE_PRIVATE)
-
-        getUpDelayPicker = findViewById(R.id.getUpDelayPicker)
-        getUpDelayPicker.maxValue = 60
-        getUpDelayPicker.minValue = 1
-        val getUpDelayMinutes =
-            (sharedPrefs.getLong(getString(R.string.GET_UP_DELAY_KEY), 5) / 60000L).toInt()
-        getUpDelayPicker.value = getUpDelayMinutes
-
-        nextButton = findViewById(R.id.set_getup_delay)
-        nextButton.setOnClickListener {
-            val intent = Intent(this, StayOffActivity::class.java)
-            setGetUpDelayTime()
-            startActivity(intent)
-        }
-
+        setContentView(R.layout.main_content_host)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        if (savedInstanceState == null) {
+            val fragment = GetUpDelayFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.main_content, fragment)
+                .commit()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -52,16 +32,6 @@ class GetUpDelayActivity : AppCompatActivity() {
         }
         R.id.streaks_menu_option -> openStreaksScreen()
         else -> super.onOptionsItemSelected(item)
-    }
-
-    private fun setGetUpDelayTime() {
-        val time = getUpDelayPicker.value * 60000L
-        val sharedPrefs =
-            getSharedPreferences(getString(R.string.shared_prefs_name), Context.MODE_PRIVATE)
-        with(sharedPrefs.edit()) {
-            putLong(getString(R.string.GET_UP_DELAY_KEY), time)
-            apply()
-        }
     }
 
     private fun openStreaksScreen(): Boolean {
---
+++ app/src/main/java/me/colinmarsch/dawn/GetUpDelayFragment.kt
@@ -0,0 +1,56 @@
+package me.colinmarsch.dawn
+
+import android.app.AlarmManager
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.NumberPicker
+import androidx.fragment.app.Fragment
+
+class GetUpDelayFragment: Fragment() {
+    private lateinit var alarmManager: AlarmManager
+    private lateinit var getUpDelayPicker: NumberPicker
+    private lateinit var nextButton: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.getup_delay_layout, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        alarmManager = view.context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val sharedPrefs =
+            view.context.getSharedPreferences(getString(R.string.shared_prefs_name), Context.MODE_PRIVATE)
+
+        getUpDelayPicker = view.findViewById(R.id.getUpDelayPicker)
+        getUpDelayPicker.maxValue = 60
+        getUpDelayPicker.minValue = 1
+        val getUpDelayMinutes =
+            (sharedPrefs.getLong(getString(R.string.GET_UP_DELAY_KEY), 5) / 60000L).toInt()
+        getUpDelayPicker.value = getUpDelayMinutes
+
+        nextButton = view.findViewById(R.id.set_getup_delay)
+        nextButton.setOnClickListener {
+            // TODO(colinmarsch): move this to the activity
+            val intent = Intent(view.context, StayOffActivity::class.java)
+            setGetUpDelayTime(sharedPrefs)
+            startActivity(intent)
+        }
+    }
+
+    private fun setGetUpDelayTime(sharedPrefs: SharedPreferences) {
+        val time = getUpDelayPicker.value * 60000L
+        with(sharedPrefs.edit()) {
+            putLong(getString(me.colinmarsch.dawn.R.string.GET_UP_DELAY_KEY), time)
+            apply()
+        }
+    }
+}
